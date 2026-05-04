13203cd60fea69509712a2b51347db0bb1154a4f
akira_t1-admin <mot117mot@gmail.com>
2026-05-05 00:24:17 +0900

    [Add] 実際の地形をタイルマップで生成+タイルマップに沿ったプレイヤーの移動を仮実装(GetKeyDownを使っているためActionMapを使った方に要変更)

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Player : MonoBehaviour
+{
+    public int MaxHP;
+    public int HP;
+
+    public int x;
+    public int y;
+
+    public List<int> Items;
+}
